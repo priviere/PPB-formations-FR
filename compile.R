b9f6358231abf0266b7ce5fe10f626d133702ca5
priviere <pierre@semencespaysannes.org>
2016-10-14 08:48:14 +0200

    update

--- compile.R
+++ compile.R
@@ -4,5 +4,6 @@ system("pdflatex -no-file-line-error -interaction=nonstopmode cycle_formations.t
 system("biblatex cycle_formations.tex")
 system("pdflatex -no-file-line-error -interaction=nonstopmode cycle_formations.tex")
 system("cp cycle_formations.pdf /home/pierre/Dropbox/PPB_formations_FR/")
+setwd("../")
 
 
